b09f07fc1546d039b95711329ddeb78c17af61ca
msnliu <zongjunliu@hsph.harvard.edu>
2022-05-24 17:07:47 -0400

    finished lab01

--- lab/lab01/ll_cycle.c
+++ lab/lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    if (!head) {
+	return 0;
+    }
+    while (hare->next && hare->next->next) {
+    	hare = hare->next->next;
+	tortoise = tortoise->next;
+	if (hare == tortoise) {
+	    return 1;
+	}
+    }
     return 0;
 }
